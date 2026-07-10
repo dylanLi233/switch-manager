1b10e12d61c69afb547a74b82831aa3316754104
dylanLi233 <18625787756@163.com>
2026-07-10 20:27:18 +0800

    fix(db): make inventory rollback fail on schema drift

--- migrations/000002_switch_inventory.down.sql
+++ migrations/000002_switch_inventory.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS plugin_registry;
-DROP TABLE IF EXISTS switch_capabilities;
-DROP TABLE IF EXISTS switches;
-DROP TABLE IF EXISTS credentials;
+DROP TABLE plugin_registry;
+DROP TABLE switch_capabilities;
+DROP TABLE switches;
+DROP TABLE credentials;
