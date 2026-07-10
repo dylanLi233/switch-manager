6ca0708a029e413da16fc1d095729a24d57a6851
dylanLi233 <18625787756@163.com>
2026-07-10 20:20:04 +0800

    feat(db): add tasks audit and backups down migration

---
+++ migrations/000003_tasks_audit_backups.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS config_backups;
+DROP TABLE IF EXISTS audit_logs;
+DROP TABLE IF EXISTS batch_task_items;
+DROP TABLE IF EXISTS batch_tasks;
+DROP TABLE IF EXISTS task_executions;
+DROP TABLE IF EXISTS tasks;
