6372646bd6c6aafa817aa24ec4b248f6eb978f4f
dylanLi233 <18625787756@163.com>
2026-07-13 11:54:59 +0800

    TASK-013: add standard VLAN operation identifiers

---
+++ pkg/pluginapi/standard_operations.go
@@ -0,0 +1,9 @@
+package pluginapi
+
+const (
+	OperationVLANList   OperationName = "vlan.list"
+	OperationVLANGet    OperationName = "vlan.get"
+	OperationVLANCreate OperationName = "vlan.create"
+	OperationVLANUpdate OperationName = "vlan.update"
+	OperationVLANDelete OperationName = "vlan.delete"
+)
